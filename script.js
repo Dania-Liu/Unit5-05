848f4fdc794920777636f617ac6a4b535aae2c82
Dania <dania.liu@ocsbstudent.ca>
2021-03-18 14:55:52 +0000

    Fixed JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// 
+// Sets root as 5 and answer as 0.
 let root = 5
 let answer = 0
 
+// Click on button to calculate square.
 document.getElementById('button').addEventListener('click', square)
 
+// Gives an alert with the answer inserted into the input box.
 function square () {
   root = document.getElementById('input').value
-
   answer = root * root
-
   alert(answer)
-
 }
